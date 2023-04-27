fcb01e377f5e425ceb1d9fe426c413d2083b05c6
GonzaloLR15 <115282265+Macumba45@users.noreply.github.com>
2023-04-27 12:08:41 +0200

    docs: create folder and theme's project

---
+++ src/router/index.tsx
@@ -0,0 +1,16 @@
+import { FC, memo } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Landing from '../views/landing';
+
+const Router: FC = () => {
+
+     return (
+          <BrowserRouter>
+               <Routes>
+                    <Route path="/" element={<Landing />} />
+               </Routes>
+          </BrowserRouter>
+     );
+};
+
+export default memo(Router);
---
+++ src/views/landing.tsx
@@ -0,0 +1,20 @@
+import { FC, memo } from "react";
+
+
+
+
+
+const Landing: FC = () => {
+
+
+    return (
+
+
+
+        <div>ESTO ES LA LANDING</div>
+    )
+}
+
+
+
+export default memo(Landing)
