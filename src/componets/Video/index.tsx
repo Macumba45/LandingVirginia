05dad564f3bc50677dae367f53cf07240065334a
Macumba45 <gonzalolovo@gmail.com>
2023-04-28 12:45:29 +0200

    styles: new animations flow

--- src/componets/Video/index.tsx
+++ src/componets/Video/index.tsx
@@ -1,33 +1,123 @@
 import { FC, memo } from "react";
+import { motion } from "framer-motion";
 import {
-  MainContainer,
-  TitleContainer,
-  Title,
-  VideoContainer,
-  VideoUrl,
-  Span,
+    MainContainer,
+    TitleContainer,
+    Title,
+    VideoContainer,
+    VideoUrl,
+    Span,
 } from "./styles";
 
 const Video: FC = () => {
-  return (
-    <MainContainer>
-      <TitleContainer>
-        <Title>
-          Descubre la nueva profesión con la que puedes generar entre
-        </Title>
-        <Span
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0, scale: 1.1, color: "#fbfbfb" }}
-          transition={{ duration: 0.5 }}
-        >
-          3000€ y 5000€ al mes
-        </Span>
-      </TitleContainer>
-      <VideoContainer>
-        <VideoUrl src="/videos/myvideo.mp4" controls />
-      </VideoContainer>
-    </MainContainer>
-  );
+
+    const titleVariants = {
+        hidden: {
+            opacity: 0,
+        },
+        visible: {
+            opacity: 1,
+            transition: {
+                delay: 0.05, // retrasa el inicio de la animación medio segundo
+                staggerChildren: 0.04, // retrasa el inicio de cada letra 0.1 segundos
+            },
+        },
+    };
+
+    const letterVariants = {
+        hidden: {
+            opacity: 0,
+            y: 50, // desplaza cada letra hacia abajo mientras está oculta
+        },
+        visible: {
+            opacity: 1,
+            y: 0, // devuelve cada letra a su posición original
+        },
+    };
+
+
+    return (
+        <MainContainer>
+            <TitleContainer>
+                <motion.div
+                    variants={titleVariants}
+                    initial="hidden"
+                    animate="visible"
+                >
+                    <motion.span variants={letterVariants}>D</motion.span>
+                    <motion.span variants={letterVariants}>e</motion.span>
+                    <motion.span variants={letterVariants}>s</motion.span>
+                    <motion.span variants={letterVariants}>c</motion.span>
+                    <motion.span variants={letterVariants}>u</motion.span>
+                    <motion.span variants={letterVariants}>b</motion.span>
+                    <motion.span variants={letterVariants}>r</motion.span>
+                    <motion.span variants={letterVariants}>e</motion.span>
+                    <motion.span variants={letterVariants}> </motion.span>
+                    <motion.span variants={letterVariants}>l</motion.span>
+                    <motion.span variants={letterVariants}>a</motion.span>
+                    <motion.span variants={letterVariants}> </motion.span>
+                    <motion.span variants={letterVariants}>n</motion.span>
+                    <motion.span variants={letterVariants}>u</motion.span>
+                    <motion.span variants={letterVariants}>e</motion.span>
+                    <motion.span variants={letterVariants}>v</motion.span>
+                    <motion.span variants={letterVariants}>a</motion.span>
+                    <motion.span variants={letterVariants}> </motion.span>
+                    <motion.span variants={letterVariants}>p</motion.span>
+                    <motion.span variants={letterVariants}>r</motion.span>
+                    <motion.span variants={letterVariants}>o</motion.span>
+                    <motion.span variants={letterVariants}>f</motion.span>
+                    <motion.span variants={letterVariants}>e</motion.span>
+                    <motion.span variants={letterVariants}>s</motion.span>
+                    <motion.span variants={letterVariants}>i</motion.span>
+                    <motion.span variants={letterVariants}>ó</motion.span>
+                    <motion.span variants={letterVariants}>n</motion.span>
+                    <motion.span variants={letterVariants}> </motion.span>
+                    <motion.span variants={letterVariants}>c</motion.span>
+                    <motion.span variants={letterVariants}>o</motion.span>
+                    <motion.span variants={letterVariants}>n</motion.span>
+                    <motion.span variants={letterVariants}> </motion.span>
+                    <motion.span variants={letterVariants}>l</motion.span>
+                    <motion.span variants={letterVariants}>a</motion.span>
+                    <motion.span variants={letterVariants}> </motion.span>
+                    <motion.span variants={letterVariants}>q</motion.span>
+                    <motion.span variants={letterVariants}>u</motion.span>
+                    <motion.span variants={letterVariants}>e</motion.span>
+                    <motion.span variants={letterVariants}> </motion.span>
+                    <motion.span variants={letterVariants}>p</motion.span>
+                    <motion.span variants={letterVariants}>u</motion.span>
+                    <motion.span variants={letterVariants}>e</motion.span>
+                    <motion.span variants={letterVariants}>d</motion.span>
+                    <motion.span variants={letterVariants}>e</motion.span>
+                    <motion.span variants={letterVariants}>s</motion.span>
+                    <motion.span variants={letterVariants}> </motion.span>
+                    <motion.span variants={letterVariants}>g</motion.span>
+                    <motion.span variants={letterVariants}>a</motion.span>
+                    <motion.span variants={letterVariants}>n</motion.span>
+                    <motion.span variants={letterVariants}>a</motion.span>
+                    <motion.span variants={letterVariants}>r</motion.span>
+                    <motion.span variants={letterVariants}> </motion.span>
+                    <motion.span variants={letterVariants}>e</motion.span>
+                    <motion.span variants={letterVariants}>n</motion.span>
+                    <motion.span variants={letterVariants}>t</motion.span>
+                    <motion.span variants={letterVariants}>r</motion.span>
+                    <motion.span variants={letterVariants}>e</motion.span>
+                    <motion.span variants={letterVariants}>.</motion.span>
+                    <motion.span variants={letterVariants}>.</motion.span>
+                    <motion.span variants={letterVariants}>.</motion.span>
+                </motion.div>
+                <Span
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0, scale: 1.1, color: "#fbfbfb" }}
+                    transition={{ duration: 2.0 }}
+                >
+                    3000€ y 5000€ al mes
+                </Span>
+            </TitleContainer>
+            <VideoContainer>
+                <VideoUrl src="/videos/myvideo.mp4" controls />
+            </VideoContainer>
+        </MainContainer>
+    );
 };
 
 export default memo(Video);
--- src/componets/Video/styles.ts
+++ src/componets/Video/styles.ts
@@ -3,12 +3,24 @@ import styled from "styled-components";
 
 export const MainContainer = styled.div``;
 
-export const TitleContainer = styled.div`
+export const TitleContainer = styled(motion.div)`
   display: flex;
   justify-content: center;
   flex-direction: column;
-  margin-top: 5rem;
-  margin-bottom: 5rem;
+  margin-top: 3rem;
+  margin-bottom: 3rem;
+  font-family: ${({ theme }) => theme.fonts.montserrat};
+  color: ${({ theme }) => theme.colors.primaryLight};
+  font-size: ${({ theme }) => theme.fontSizes.medium};
+  text-align: center;
+  margin-left: 1rem;
+  margin-right: 1rem;
+  line-height: 5rem;
+
+  @media screen and (max-width: 600px) {
+    font-size: ${({ theme }) => theme.fontSizes.small};
+    line-height: 2rem;
+  }
 `;
 
 export const Title = styled.h1`
@@ -48,7 +60,7 @@ export const Span = styled(motion.span)`
   font-weight: 800;
 
   @media screen and (max-width: 600px) {
-    font-size: ${({ theme }) => theme.fontSizes.small};
+    font-size: 1.8rem;
     line-height: 2rem;
   }
 `;
